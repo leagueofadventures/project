5725cdeded8e7d29fa429625ded9074ce0ebaf48
leagueofadventures <freezon.yt3@gmail.com>
2025-02-11 10:33:53 +0300

    Add files via upload

--- набор тайлов.tsx
+++ набор тайлов.tsx
@@ -1,5 +1,5 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.11.2" name="набор тайлов" tilewidth="64" tileheight="64" tilecount="3" columns="0">
+<tileset version="1.10" tiledversion="1.11.2" name="набор тайлов" tilewidth="64" tileheight="64" tilecount="4" columns="0">
  <grid orientation="orthogonal" width="1" height="1"/>
  <tile id="0">
   <image source="../Downloads/трава (2).jpg" width="64" height="64"/>
@@ -9,5 +9,8 @@
  </tile>
  <tile id="2">
   <image source="../Downloads/камень.png" width="64" height="64"/>
+ </tile>
+ <tile id="5">
+  <image source="../Downloads/пруд (1) (1).jpg" width="64" height="64"/>
  </tile>
 </tileset>
